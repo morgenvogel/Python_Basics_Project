05f7f39018e0213c917e7a44897c6ece7f3d1c60
morgenvogel <morgenvogel@yandex.ru>
2022-06-19 17:57:31 +0300

    Errors corrected

--- Lesson 3/Lesson 3.py
+++ Lesson 3/Lesson 3.py
@@ -5,31 +5,43 @@ def div(a, b):
         print('Division by zero is forbidden!')
     except ValueError:
         print('You should enter integers!')
+
+
 print(div(input('Input first digital value: '), input('Input second digital value: ')))
+
 
 def print_data(**kwargs):
     return ' '.join(kwargs.values())
+
+
 print(print_data(name=input('Enter your name: '), surname=input('Enter your surname: '), birth_year=input('Enter your year of birth: '), city=input('Enter your city: '), email=input('Enter your email: '), phone=input('Input your phone number: ')))
+
 
 def my_func(a, b, c):
     try:
         return sum((a, b, c)) - min(a, b, c)
     except TypeError:
         return 'Enter only numbers!'
+
+
 print(my_func(10, 10, 2))
+
 
 def my_func(x, y):
     try:
         x = float(x)
-        f = int(y)
+        y = int(y)
     except ValueError:
-        print('Программа работает тольок с действительными x и целыми y!')
+        print('Программа работает только с действительными x и целыми y!')
         return
     if x <= 0 or y >= 0:
         print('Программа работает только с положительными x и отрицательными y!')
         return
     return x ** y
+
+
 print(round(my_func(2, -2), 10))
+
 
 def pow_negative(x, y):
     try:
@@ -45,11 +57,14 @@ def pow_negative(x, y):
     for _ in range(abs(y)):
         result /= x
     return result
+
+
 print(round(pow_negative(input('Введите действительный положительный x: '), input('Введите целый отрицательный y: ')), 10))
+
 
 def my_func():
     s = 0
-    in_list = input('Введите число или Q для выхода: ').upper().split()
+    in_list = input('Введите числа через пробел или Q для выхода: ').upper().split()
     for i in in_list:
         if i == 'Q':
             return s, True
@@ -57,7 +72,9 @@ def my_func():
             s += int(i)
         except ValueError:
             pass
-        return s, False
+    return s, False
+
+
 res = 0
 while True:
     a, b = my_func()
@@ -66,12 +83,17 @@ while True:
     if b:
         break
 
+
 def int_func(word):
     return word[0].upper() + word[1:].lower()
+
+
 s = input().split()
 for i, word in enumerate(s):
     if not word.isascii() or not word.isalpha or not word.islower():
         print('Error!!!')
     else:
         s[i] = int_func(word)
+
+
 print(' '.join(s))
